b97a9843d9c8ff6b1ae08e94232f8c0ff685ab72
hetianyuanjames <hty1016@ad.unc.edu>
2022-03-28 00:11:53 -0400

    updated

--- flips.js
+++ flips.js
@@ -1,14 +1,9 @@
 import { coinFlips, countFlips } from "./modules/coin.mjs";
 import minimist from "minimist";
 
-let args = minimist(process.argv.slice(2))
-
-if (args['number']) {
-    let results = coinFlips(args['number']);
-    console.log(results);
-    console.log(countFlips(results));
-} else {
-    let results = coinFlips(1)
-    console.log(results);
-    console.log(countFlips(results));
-}
+const args = minimist(process.argv.slice(2))
+if (args.number == null) 
+    args.number = 1;
+let record = coinFlips(args.number);
+console.log(record);
+console.log(countFlips(record));
--- guess-flip.js
+++ guess-flip.js
@@ -1,19 +1,21 @@
 import { flipACoin } from "./modules/coin.mjs";
+import minimist from "minimist";
 
-var args = process.argv.slice(2);
-if (args.length == 0){
-    console.log("Error: no input.");
-    console.log("Usage: node guess-flip --call=[heads|tails]");
-}
-else if (args!='--call=tails' && args!='--call=heads'){
-    console.log("Error: no input.");
-    console.log("Usage: node guess-flip --call=[heads|tails]");
-}
-else{
-    if (args == '--call=tails'){
-        console.log(flipACoin('tails'))
+try {
+    const argv = process.argv.slice(2)[0].slice(7);
+    if (argv == 'heads' || argv == 'tails')
+        console.log(flipACoin(argv)); 
+    else
+        throw new Error;
+} 
+catch (error) {
+    console.log('Error: no input\nUsage: node guess-flip --call=[heads|tails]')
+    const args = minimist(process.argv.slice(2));
+    if (args.call == null || (args.call != "heads" && args.call != "tails")) {
+        console.log("Error: no input.");
+        console.log("Usage: node guess-flip --call=[heads|tails]");
     }
-    else{
-        console.log(flipACoin('heads'))
+    else {
+        console.log(flipACoin(args.call));
     }
 }
